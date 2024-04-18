cd32d6b3240d9a705b8856a62b3aa387a4e701fd
Erin McLaughlin <erinmclaughlin219@gmail.com>
2024-04-17 23:46:59 -0400

    a real chat!

--- src/Site/Site/Components/TerminalChat.razor.cs
+++ src/Site/Site/Components/TerminalChat.razor.cs
@@ -1,25 +1,42 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Site.AI;
 
 namespace Site.Components;
+
+public class ChatMessage(string author)
+{
+    public string Author { get; } = author;
+    public string Message { get; set; } = string.Empty;
+}
 
 public sealed partial class TerminalChat
 {
     private readonly CancellationTokenSource _ctSource = new();
+    private readonly OpenAIPromptExecutionSettings _promptExecutionSettings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
 
     [Inject]
     private Kernel Kernel { get; set; } = default!;
 
+    [Inject]
+    private NavigationManager NavigationManager { get; set; } = default!;
+
     private ChatHistory ChatHistory { get; } = [];
-    private List<(string AuthorName, string Message)> Messages { get; } = [];
+    private List<ChatMessage> Messages { get; } = [];
     private ChatState State { get; set; } = ChatState.Loading;
 
     protected override void OnInitialized()
     {
+        Kernel.Plugins.AddFromObject(new WebsitePlugin(NavigationManager));
+
         ChatHistory.AddSystemMessage("You are an AI assistant on Erin McLaughlin's personal website & portfolio. Erin is the software engineer that programmed you. Talk to users about her.");
-        ChatHistory.AddAssistantMessage("Oh hey, you found my website! What would you like to know?");
-        Messages.Add(("AI", ChatHistory.Last().Content ?? "No comment."));
+        ChatHistory.AddAssistantMessage("Oh hey, you found Erin's website! What would you like to know?");
+        Messages.Add(new ChatMessage("AI")
+        {
+            Message = ChatHistory.Last().Content ?? ""
+        });
         StateHasChanged();
 
         State = ChatState.WaitingForUser;
@@ -28,7 +45,7 @@ public sealed partial class TerminalChat
     private async Task RenderUserMessage(string message)
     {
         ChatHistory.AddUserMessage(message);
-        Messages.Add(("User", message));
+        Messages.Add(new ("User") { Message = message });
         State = ChatState.WaitingForAssistant;
         StateHasChanged();
 
@@ -37,24 +54,24 @@ public sealed partial class TerminalChat
 
     private async Task RenderAssistantMessage()
     {
-        var chatService = Kernel.GetRequiredService<IChatCompletionService>();
-        var response = await chatService.GetChatMessageContentAsync(ChatHistory, null, Kernel, _ctSource.Token);
+        State = ChatState.StreamingAssistantMessage;
 
-        var content = response.Content ?? "Hrm. Looks like I've encountered an error. Sorry about that! Maybe try back later?";
-        Messages.Add(("User", content));
-        ChatHistory.AddAssistantMessage(content);
+        var message = new ChatMessage("AI");
+        Messages.Add(message);
+        var chatService = Kernel.GetRequiredService<IChatCompletionService>();
+        var response = chatService.GetStreamingChatMessageContentsAsync(ChatHistory, _promptExecutionSettings, Kernel, _ctSource.Token);
+
+        await foreach(var part in response)
+        {
+            message.Message += part.Content ?? "";
+            await Task.Delay(50);
+            StateHasChanged();
+        }
+
+        ChatHistory.AddAssistantMessage(message.Message);
 
         State = ChatState.WaitingForUser;
         StateHasChanged();
-    }
-
-    private IEnumerable<(string AuthorName, string Message)> GetChatHistory()
-    {
-        foreach (var message in ChatHistory)
-        {
-            if (message.AuthorName is not null && message.Content is not null)
-            yield return (message.AuthorName, message.Content);
-        }
     }
 
     private enum ChatState
