322db310190d20ddf2b28268b7f3cebddc05086b
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-04-15 06:29:48 -0400

    add wasm project

---
+++ PersonalWebsite/Extensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.AI;
+using OpenAI;
+
+namespace PersonalWebsite;
+
+public static class Extensions
+{
+    public static void AddBlazorComponents(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+    }
+
+    public static void AddChatClient(this WebApplicationBuilder builder)
+    {
+        var chatClient = new OpenAIClient(builder.Configuration["OpenAI:Key"]).GetChatClient("gpt-4o-mini");
+        builder.Services.AddChatClient(chatClient.AsIChatClient()).UseFunctionInvocation().UseLogging();
+    }
+}
--- PersonalWebsite/Program.cs
+++ PersonalWebsite/Program.cs
@@ -1,16 +1,18 @@
-﻿using Microsoft.Extensions.AI;
-using OpenAI;
+﻿using PersonalWebsite;
 using PersonalWebsite.Components;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
-var chatClient = new OpenAIClient(builder.Configuration["OpenAI:Key"]).GetChatClient("gpt-4o-mini");
-builder.Services.AddChatClient(chatClient.AsIChatClient()).UseFunctionInvocation().UseLogging();
+builder.AddBlazorComponents();
+builder.AddChatClient();
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
@@ -22,6 +24,8 @@ app.UseAntiforgery();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(PersonalWebsite.Client._Imports).Assembly);
 
 app.Run();
